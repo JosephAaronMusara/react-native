a4ba6cccdaa2732377413e55535ed669e3111a37
Joseph Aaron Musara <josephaaronmusara@gmail.com>
2024-07-08 14:25:38 +0200

    Starter. With object state too

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const [name, setName] = useState('Joseph Aaron');
+  const [person, setPerson] = useState({name: 'Tariroyashe Aaron',age : 17});
+
+  const clickHandler = () => {
+    setName('Tariroyashe Aaron');
+    setPerson({name: 'Joseph Aaron', age : 21});
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text>My name is {name}</Text>
+      </View>
+      <View style={styles.buttonContainer}>
+        <Text>Im in love with {person.name} , who is {person.age} years old.</Text>
+        <Text></Text>
+          <Button title='Update state' onPress={clickHandler}/>
+       </View>
     </View>
   );
 }
@@ -16,5 +30,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  buttonContainer:{
+    marginTop:20,
   },
 });
